a75bb59ba864e36a7e98f17234344339d1c23c80
andyDevMdg <andyavotiana@gmail.com>
2023-08-03 22:54:36 +0300

    Add preventDefault to the Add button

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,9 +2,18 @@ import React , { useState }from 'react'
 
 function TodoForm() {
     const [value, setValue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        console.log(value)
+    }
   return (
-    <form className='todoForm'>
-        <input type="text" className='todoInput' placeholder='What do you plan to do ?'/>
+    <form className='todoForm' onSubmit={handleSubmit}>
+        <input type="text" 
+        className='todoInput' 
+        placeholder='What do you plan to do ?' 
+        onChange={(e) => setValue(e.target.value)}/>
         <button type='submit' className='todoBtn'>Add</button>
     </form>
   )
