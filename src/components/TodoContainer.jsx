c543d02d6fb210544bd26f90603803e299dec7bc
andyDevMdg <andyavotiana@gmail.com>
2023-08-04 08:07:23 +0300

    Add completed task feature

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -18,12 +18,17 @@ function TodoContainer() {
     console.log(todos)
   }
 
+  const toggleComplete = id => {
+    setTodos(todos.map(todo => todo.id === id ?
+      {...todo, completed: !todo.completed} : todo))
+  }
   return (
     <div className='todoContainer'>
       <h1>Your todos</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} />
+        <Todo task={todo} key={index}
+          toggleComplete={toggleComplete} />
       ))}
     </div>
   )
