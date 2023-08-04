0e82c4320527be8679f03dd0d2e883b42406de01
andyDevMdg <andyavotiana@gmail.com>
2023-08-04 07:57:47 +0300

    Add task section with edit & delete button

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,23 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import { v4 as uuidv4 } from 'uuid';
+import Todo from './Todo';
 uuidv4();
 
 function TodoContainer() {
   const [todos, setTodos] = useState([])
 
   const addTodo = todo => {
-    setTodos([...todos, 
-      {id: uuidv4(), 
-      task: todo, 
-      completed: false, 
-      isEditing: false}])
-      console.log(todos)
+    setTodos([...todos,
+    {
+      id: uuidv4(),
+      task: todo,
+      completed: false,
+      isEditing: false
+    }])
+    console.log(todos)
   }
 
   return (
     <div className='todoContainer'>
-      <TodoForm addTodo={addTodo}/>
+      <h1>Your todos</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
     </div>
   )
 }
